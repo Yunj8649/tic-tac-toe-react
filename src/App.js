b9e3d466cb54f98377e67065604662667dcbbc4e
yunj <yunj8649@xingxingmobility.com>
2021-10-29 02:16:03 +0900

    추가개발건 2까지 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 /* eslint-disable react/button-has-type */
 import React, { useMemo, useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 import './App.css';
+
+const ROW = 3;
+const COLUMN = 3;
+
+/** 추가 개발 건 */
+// 1. 이동 기록 목록에서 특정 형식(행, 열)으로 각 이동의 위치를 표시해주세요.
+// 2. 이동 목록에서 현재 선택된 아이템을 굵게 표시해주세요.
+// 3. 사각형들을 만들 때 하드코딩 대신에 두 개의 반복문을 사용하도록 Board를 다시 작성해주세요.
+// 4. 오름차순이나 내림차순으로 이동을 정렬하도록 토글 버튼을 추가해주세요.
+// 5. 승자가 정해지면 승부의 원인이 된 세 개의 사각형을 강조해주세요.
+// 6. 승자가 없는 경우 무승부라는 메시지를 표시해주세요.
 
 function Square( props ) {
     const { value, onClick } = props;
@@ -15,37 +27,19 @@ function Square( props ) {
     );
 }
 
-function Board() {
-    const [ squares, setSquares ] = useState( Array( 9 ).fill( null ) );
-    const [ xIsNext, setXIsNext ] = useState( true );
-    const winner = calculateWinner( squares );
-    let status;
-    if ( winner ) {
-        status = `Winner: ${ winner }`;
-    } else {
-        status = `Next player: ${ xIsNext ? 'X' : 'O' }`;
-    }
+function Board( props ) {
+    // const [ squares, setSquares ] = useState( Array( 9 ).fill( null ) );
+    // const [ xIsNext, setXIsNext ] = useState( true );
 
-    const handleClick = ( i ) => {
-        // .slice()를 호출하는 것으로 기존 배열을 수정하지 않고 squares 배열의 복사본을 생성하여 수정
-        const squaresTemp = squares.slice();
-        if ( calculateWinner( squares ) || squares[i]) {
-            return;
-        }
-        squaresTemp[i] = xIsNext ? 'X' : 'O';
-        setSquares( squaresTemp );
-        setXIsNext( !xIsNext );
-    };
     const renderSquare = ( i ) => (
         <Square
-            value={ squares[i] }
-            onClick={ () => handleClick( i ) }
+            value={ props.squares[i] }
+            onClick={ () => props.onClick( i ) }
         />
     );
 
     return (
-        <div>
-            <div className="status">{status}</div>
+        <div className="board-box">
             <div className="board-row">
                 {renderSquare( 0 )}
                 {renderSquare( 1 )}
@@ -66,14 +60,73 @@ function Board() {
 }
 
 function Game() {
+    const [ history, setHistory ] = useState([ {
+        squares: Array( ROW * COLUMN ).fill( null ),
+        coordinates: [],
+    } ]);
+    const [ xIsNext, setXIsNext ] = useState( true );
+    const [ stepNumber, setStepNumber ] = useState( 0 );
+
+    const current = history[stepNumber];
+
+    const moves = history.map( ( step, move ) => {
+        const desc = move
+            ? `Go to move #${ move } [${ history[move].coordinates.x }, ${ history[move].coordinates.y }]`
+            : 'Go to game start';
+        return (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={ move }>
+                <button
+                    className={ ( stepNumber === move ) ? 'move-history-bold' : undefined }
+                    onClick={ () => jumpTo( move ) }
+                >
+                    {desc}
+                </button>
+            </li>
+        );
+    });
+
+    const winner = calculateWinner( current.squares );
+    let status;
+    if ( winner ) {
+        status = `Winner: ${ winner }`;
+    } else {
+        status = `Next player: ${ xIsNext ? 'X' : 'O' }`;
+    }
+
+    const handleClick = ( i ) => {
+        const historyT = history.slice( 0, stepNumber + 1 );
+        const squares = current.squares.slice();
+        const x = Math.floor( i / ROW ) + 1;
+        const y = Math.floor( i % COLUMN ) + 1;
+        const coordinates = { x, y };
+        // .slice()를 호출하는 것으로 기존 배열을 수정하지 않고 squares 배열의 복사본을 생성하여 수정
+        // const squaresTemp = squares.slice();
+        if ( calculateWinner( squares ) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory( historyT.concat([ { squares, coordinates } ]) );
+        setStepNumber( historyT.length );
+        setXIsNext( !xIsNext );
+    };
+
+    const jumpTo = ( step ) => {
+        setStepNumber( step );
+        setXIsNext( ( step % 2 ) === 0 );
+    };
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    squares={ current.squares }
+                    onClick={ ( i ) => handleClick( i ) }
+                />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div className="status">{status}</div>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
